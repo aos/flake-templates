85dc600b7cd1eabd9f1e3b487c766800b19fe7b1
Aos Dabbagh <aosdab@gmail.com>
2022-12-10 02:07:22 -0800

    add rust template

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
         path = ./python-poetry-template;
         description = "Simple Python + Poetry template";
       };
+      rust-minimal = {
+        path = ./rust-minimal;
+        description = "Minimal Rust flake template";
+      };
     };
   };
 }
