04009b11a42152ce9a65ecc9701a1423c80dbd81
Aos Dabbagh <aosdab@gmail.com>
2023-04-15 21:56:31 -0400

    fix flake.nix

--- go-web-server/flake.nix
+++ go-web-server/flake.nix
@@ -2,7 +2,7 @@
   description = "A basic Go web server setup";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
