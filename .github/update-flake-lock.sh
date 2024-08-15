ec642cbe7af0994a120edb5e23f117559c518413
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-08-15 18:03:27 -0400

    update-flake-lock script

---
+++ .github/update-flake-lock.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# https://github.com/DavHau/update-flake-lock/blob/main/update-flake-lock.sh
+
+update(){
+  if [[ -n "${TARGETS:-}" ]]; then
+      inputs=()
+      for input in $TARGETS; do
+          inputs+=("--update-input" "$input")
+      done
+      nix "${options[@]}" flake lock "${inputs[@]}" --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG"
+  else
+      nix "${options[@]}" flake update --commit-lock-file --commit-lockfile-summary "$COMMIT_MSG"
+  fi
+}
+
+
+options=()
+if [[ -n "${NIX_OPTIONS:-}" ]]; then
+    for option in $NIX_OPTIONS; do
+        options+=("${option}")
+    done
+fi
+
+if [[ -n "${PATH_TO_FLAKE_DIR:-}" ]]; then
+  cd "$PATH_TO_FLAKE_DIR"
+  update
+elif [[ -n "${FLAKE_DIRS:-}" ]]; then
+  for flake_dir in $FLAKE_DIRS; do
+    cd "$flake_dir"
+    update
+  done
+else
+  update
+fi
